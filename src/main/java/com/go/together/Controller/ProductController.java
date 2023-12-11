208d6de985f1301dcc02a8e96ae4657e959431aa
DESKTOP-BPFBS4R\jungkey <higggu@naver.com>
2023-12-11 20:55:40 +0900

    12/11일 상품삭제 post 방식 수정

--- src/main/java/com/go/together/Controller/ProductController.java
+++ src/main/java/com/go/together/Controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,15 +45,6 @@ public class ProductController {
         if (userNumber == null) {
             throw new IllegalArgumentException("유저아이디가 없습니다");
         }
-//        String userId = productDto.getUserId();
-//        System.out.println("아이디 받아오는지 !!@@@@@@@@@@@@@@@@@!!"+userId);
-//        if (!"admin".equals(userId)) {
-//            throw new IllegalArgumentException("admin 아이디가 없습니다");
-//        }
-
-        // 원하는 로직을 추가하여 productVo를 처리할 수 있습니다.
-        // productService를 호출하여 필요한 비즈니스 로직을 수행하고,
-        // 결과를 List<ProductVo> 형태로 반환합니다.
         System.out.println("유저번호 !!!!!!!" + userNumber);
         List<ProductVo> productVoList = productService.findAllProduct(productVo);
         return productVoList;
@@ -83,17 +75,46 @@ public class ProductController {
     }
 
 
-@PostMapping("/productDelete")
-public List<Integer> productDelete(@RequestParam(value = "productNumber", required = false) List<Long> productNumbers) {
-    List<Integer> results = new ArrayList<>();
+//@PostMapping("/productDelete")
+//public List<Integer> productDelete(@RequestBody List<Long> productNumbers) {
+//        List<Integer> results = new ArrayList<>();
+//
+//
+//
+////    for(int i = 0; i < productNumbers.size(); i++){
+////        Long productNumber = productNumbers.get(i); 아래와 같은거임.,
+//        for(Long productNumber : productNumbers){
+//            int result = productService.remove(productNumber);
+//            results.add(result);
+//        }
+//        System.out.println(results+ " 1이 나오면 성공 ");
+//        return results;
+//}
 
-    for(Long productNumber : productNumbers){
-        int result = productService.remove(productNumber);
-        results.add(result);
+    @PostMapping("/productDelete")
+    public List<Integer> productDelete(@RequestBody Map<String, List<Long>> Array) {
+        List<Long> productNumbers = Array.get("productNumber");
+        List<Integer> results = new ArrayList<>();
+
+        for(Long productNumber : productNumbers){
+            int result = productService.remove(productNumber);
+            results.add(result);
+        }
+
+        System.out.println(results + " 1이 나오면 성공 ");
+        return results;
     }
-    System.out.println(results+ " 1이 나오면 성공 ");
-    return results;
-}
+    /*
+    Map<String, List<Long>>에서 String은 Map의 키(key)의 데이터 타입을 나타냅니다.
+    이 경우, 클라이언트에서 보내는 JSON 데이터가 { "productNumbers": [/상품 번호 배열 ] } 형태이므로, 키는 "productNumbers"라는 문자열입니다.
+
+    List<Long>은 Map의 값(value)의 데이터 타입을 나타냅니다. 여기서는 상품 번호의 배열을 나타내므로 Long 타입의 리스트입니다.
+
+    따라서 Map<String, List<Long>>은 "문자열을 키로 하고, Long 타입의 리스트를 값으로 하는 Map"을 의미합니다.
+    이 Map에서 "productNumbers"라는 키로 상품 번호의 리스트를 가져올 수 있습니다.
+
+    그리고 Java에서는 정수형 데이터를 표현할 때 int와 long 두 가지 타입을 주로 사용합니다. `
+*/
 
 
     @PostMapping("/productUpdate")
