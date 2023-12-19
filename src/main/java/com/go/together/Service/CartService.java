0740f16b52b2a3ffcb2e663f352f8a4f48db4a71
DESKTOP-BPFBS4R\jungkey <higggu@naver.com>
2023-12-19 13:53:24 +0900

    12/19 여러 장바구니 담기 처리

--- src/main/java/com/go/together/Service/CartService.java
+++ src/main/java/com/go/together/Service/CartService.java
@@ -7,24 +7,30 @@ import com.go.together.Vo.CartVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final CartMapper cartMapper;
+    private final ProductMapper productMapper;
 
 
-
-    public int registerCart(CartVo cartVo){
-        if(cartVo == null){
-           throw new IllegalArgumentException("등록한 cart정보 x");
+    public int registerCart(CartVo cartVo) {
+        if (cartVo == null) {
+            throw new IllegalArgumentException("등록한 cart정보 x");
         }
-        int res=cartMapper.insertCart(cartVo);
 
+        // List<String> -> String 변환
+        String selectedColor = String.join(",", cartVo.getSelectedColor());
+        String selectedSize = String.join(",", cartVo.getSelectedSize());
 
+        cartVo.setSelectedColor(selectedColor);
+        cartVo.setSelectedSize(selectedSize);
 
-
+        int res = cartMapper.insertCart(cartVo);
 
         return res;
     }
--- src/main/java/com/go/together/Vo/CartVo.java
+++ src/main/java/com/go/together/Vo/CartVo.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Data
 @Slf4j
